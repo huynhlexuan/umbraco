4c5a320527959cd21746b8ac92be9ce7ab4e196c
Huynh Le <toifixbugdao@gmailc.om>
2025-04-28 07:15:25 +0700

    add umbraco assignment

---
+++ MyProject/Program.cs
@@ -0,0 +1,34 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(p => p.AllowAnyOrigin());
+});
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseCors();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
---
+++ MyProject/Umbraco API Controller/ListingController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace MyProject.Umbraco_API_Controller
+{
+    public class ListingController : UmbracoApiController
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+        public ListingController(IUmbracoContextAccessor umbracoContextAccessor)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        [HttpGet]
+        public object GetItems()
+        {
+            if (!_umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext) || umbracoContext.Content == null)
+            {
+                return new { Error = "Umbraco context or content cache is unavailable." };
+            }
+
+            var contentNode = umbracoContext.Content.GetById(Guid.Parse("85777f17-854d-45c1-ba62-17b7438afe9c"));
+
+            return new []
+            {
+                new
+                {
+                    Id = contentNode.Id,
+                    Name = contentNode.Name,
+                    Description = contentNode.Value<string>("description"),
+                    ImageUrl = contentNode.Value<IPublishedContent>("image")?.Url(),
+                    Brand = contentNode.Value<string>("brand"),
+                    Type = contentNode.Value<string>("type"),
+                    Width = contentNode.Value<string>("width")
+                },
+            };
+        }
+    }
+}
